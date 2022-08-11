5198437cde5fb121097a4afa8dbcda63bc22d0c5
sketodee <shobandejames@gmail.com>
2022-08-11 15:21:07 +0100

    merged james and farhan

--- e-commerce/src/components/PopularStalls/PopularStalls.jsx
+++ e-commerce/src/components/PopularStalls/PopularStalls.jsx
@@ -1,43 +1,43 @@
 import React from 'react'
 import "./PopularStalls.css"
 import Button from '../Button'
+import  pic1 from "../../images/assets/Ellipse9.png"
+import  pic2 from "../../images/assets/Ellipse10.png"
+import  pic3 from "../../images/assets/Ellipse11.png"
+import  pic4 from "../../images/assets/Ellipse12.png"
+import  pic5 from "../../images/assets/Ellipse13.png"
+import  pic6 from "../../images/assets/Ellipse14.png"
 
 
 const PopularStalls = () => {
   return (
     <div className='popular_stalls pt-3 pb-5 px-5 '>
-      <h5 className="primary py-2">Popular stalls you can order from </h5>
-      <div className='d-flex justify-content-around flex-wrap px-3'>
-           <div className='py-4'>
-           <div> <img src="assets/ellipse9.png" alt="" style={{width: "200px"}}/>  </div>
-             <Button title="Iya Risi Store" padding = "px-5 mt-3" />
+      <h5 className="primary">Popular stalls you can order from </h5>
+      <div className='d-flex justify-content-around flex-wrap'>
+           <div>
+           <div className="py-2 my-3"> <img src={pic1} alt="" width="200px" /> </div>
+             <Button title="Iya Risi Store" btnClass="px-4" />
            </div>
-           
-           <div className='py-4'>
-           <div> <img src="assets/ellipse10.png" alt="" style={{width: "200px"}}/>  </div>
-             <Button title="Naija Mart" padding = "px-5 mt-3" />
+           <div>
+           <div className="py-2 my-3"> <img src={pic2} alt="" width="200px" /> </div>
+             <Button title="Naija Mart" btnClass="px-4"/>
            </div>
-
-           <div className='py-4'>
-           <div> <img src="assets/ellipse11.png" alt="" style={{width: "200px"}}/>  </div>
-             <Button title="Iya Calabar" padding = "px-5 mt-3" />
+           <div>
+           <div className="py-2 my-3"> <img src={pic3} alt="" width="200px" /> </div>
+             <Button title="Iya Calabar" btnClass="px-4"/>
            </div>
-
-           <div className='py-4'>
-           <div> <img src="assets/ellipse12.png" alt="" style={{width: "200px"}}/>  </div>
-             <Button title="Thrive Market" padding = "px-5 mt-3" />
+           <div>
+           <div className="py-2 my-3"> <img src={pic4} alt="" width="200px" /> </div>
+             <Button title="Thrive Market" btnClass="px-4"/>
            </div>
-
-           <div className='py-4'>
-           <div> <img src="assets/ellipse13.png" alt="" style={{width: "200px"}}/>  </div>
-             <Button title="Wine Palace" padding = "px-5 mt-3" />
+           <div>
+           <div className="py-2 my-3"> <img src={pic5} alt="" width="200px" /> </div>
+             <Button title="Wine Palace" btnClass="px-4"/>
            </div>
-
-           <div className='py-4'>
-           <div> <img src="assets/ellipse14.png" alt="" style={{width: "200px"}}/>  </div>
-             <Button title="NIght Market" padding = "px-5 mt-3" />
+           <div>
+           <div className="py-2 my-3"> <img src={pic6} alt="" width="200px" /> </div>
+             <Button title="Night Market" btnClass="px-4"/>
            </div>
-
       </div>
     </div>
   )
--- e-commerce/src/pages/Home.jsx
+++ e-commerce/src/pages/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Hero from '../components/Hero/Hero';
+import Categories from "../components/Categories/Categories"
+import PopularStalls from "../components/PopularStalls/PopularStalls"
+import StartShopping from "../components/StartShopping"
+import AutoSlide from "../components/AutoSlide/AutoSlide"
 
 
 const Home = () => {
   return (
     <main>
       <Hero />
+      <div className="App">
+        <Categories />
+        <PopularStalls />
+        <StartShopping />
+        <AutoSlide />
+      </div>
     </main>
   )
 }
