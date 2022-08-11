5874dbdd0f7a36cc0e3ecba46dcb42e642f53a74
agoroone <omotagoro@gmail.com>
2022-08-11 06:28:21 +0100

    new

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
+import "./App.css";
+import LoginPage from "./pages/LoginPage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Nabar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import img from "./pages/fruit-gb.png";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Header />
+      <body style={{ backgroundImage: `url(${img})` }}>
+        <div>
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route path="/home" element={""} />
+              <Route path="/addlist" element={""} />
+            </Routes>
+            <LoginPage />
+          </BrowserRouter>
+        </div>
+      </body>
+    </>
   );
 }
 
