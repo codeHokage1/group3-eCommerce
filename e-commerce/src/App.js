d8d106d589b85047ce5d3b2d8d3d4bb04a261090
agoroone <omotagoro@gmail.com>
2022-08-19 15:50:13 +0100

    update

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,33 +1,34 @@
-// // import "./App.css";
-// // import LoginPage from "./pages/LoginPage";
-// // import "bootstrap/dist/css/bootstrap.min.css";
-// // import Navbar from "./components/NavBar";
-// // import { BrowserRouter, Routes, Route } from "react-router-dom";
-// // import img from "./pages/fruit-gb.png";
-// // import Header from "./components/Header";
-// // //import "./pages/form.css";
-// // import SignUp from "./pages/SignUp";
-// // import "./pages/signup.css";
+<<<<<<< HEAD
+import "./App.css";
+import LoginPage from "./pages/LoginPage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import img from "./pages/fruit-gb.png";
+import Header from "./components/Header";
+//import "./pages/form.css";
+import SignUp from "./pages/SignUp";
+import "./pages/signup.css";
 
 // // function App() {
-// //   return (
-// //     <body className="myBody" style={{ backgroundImage: `url(${img})` }}>
-// //       <Header />
+  return (
+    <body className="myBody" style={{ backgroundImage: `url(${img})` }}>
+      <Header />
 
-//       <div>
-//         <BrowserRouter>
-//           <Navbar />
-//           <Routes>
-//             <Route path="/home" element={""} />
-//             <Route path="/addlist" element={""} />
-//           </Routes>
-//         </BrowserRouter>
-//       </div>
-//       {/* <LoginPage /> */}
-//       <SignUp />
-//     </body>
-//   );
-// =======
+      <div>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/home" element={""} />
+            <Route path="/addlist" element={""} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+      {/* <LoginPage /> */}
+      <SignUp />
+    </body>
+  );
+=======
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import BonusTop from './components/BonusTop/BonusTop'
@@ -40,8 +41,6 @@ import Help from '../src/pages/Help'
 import Products from './pages/Products'
 import SingleProduct from './pages/SingleProductPage';
 import Footer2 from './components/Footer2/Footer2'
-import LoginPage from "./pages/LoginPage";
-import SignUp from "./pages/SignUp";
 
 function App() {
   return (
@@ -57,8 +56,6 @@ function App() {
         <Route path={'/products/:id'} element={<SingleProduct />} />
       </Routes>
       <Footer2 />
-          {/* <LoginPage /> */}
-      <SignUp />
     </BrowserRouter>
   )
 }
