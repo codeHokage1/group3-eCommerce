04ceb82ef0fd21ec27cafdd6d0c997e673c59e67
agoroone <omotagoro@gmail.com>
2022-08-20 09:45:08 +0100

    update on profile

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,46 +1,16 @@
-<<<<<<< HEAD
 import "./App.css";
-import LoginPage from "./pages/LoginPage";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/NavBar";
+import BonusTop from "./components/BonusTop/BonusTop";
+import NavBar from "./components/NavBar/NavBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import img from "./pages/fruit-gb.png";
-import Header from "./components/Header";
-//import "./pages/form.css";
-import SignUp from "./pages/SignUp";
-import "./pages/signup.css";
-
-// // function App() {
-  return (
-    <body className="myBody" style={{ backgroundImage: `url(${img})` }}>
-      <Header />
-
-      <div>
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route path="/home" element={""} />
-            <Route path="/addlist" element={""} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-      {/* <LoginPage /> */}
-      <SignUp />
-    </body>
-  );
-=======
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import BonusTop from './components/BonusTop/BonusTop'
-import NavBar from './components/NavBar/NavBar'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from '../src/pages/Home'
-import Account from '../src/pages/Account/Account'
-import Checkout from '../src/pages/Checkout'
-import Help from '../src/pages/Help'
-import Products from './pages/Products'
-import SingleProduct from './pages/SingleProductPage';
-import Footer2 from './components/Footer2/Footer2'
+import Home from "../src/pages/Home";
+import Account from "../src/pages/Account/Account";
+import Checkout from "../src/pages/Checkout";
+import Help from "../src/pages/Help";
+import Products from "./pages/Products";
+import SingleProduct from "./pages/SingleProductPage";
+import Footer2 from "./components/Footer2/Footer2";
+import Profile from "./pages/Account/Profile";
 
 function App() {
   return (
@@ -48,16 +18,18 @@ function App() {
       <BonusTop />
       <NavBar />
       <Routes>
-        <Route path={'/'} element={<Home />} />
-        <Route path={'/account'} element={<Account />} />
-        <Route path={'/cart'} element={<Checkout />} />
-        <Route path={'/help'} element={<Help />} />
-        <Route path={'/products'} element={<Products />} />
-        <Route path={'/products/:id'} element={<SingleProduct />} />
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/account"} element={<Account />} />
+        <Route path={"/cart"} element={<Checkout />} />
+        <Route path={"/help"} element={<Help />} />
+        <Route path={"/products"} element={<Products />} />
+        <Route path={"/products/:id"} element={<SingleProduct />} />
+        <Route path={"/profile"} element={<Profile />} />
       </Routes>
+
       <Footer2 />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- e-commerce/src/pages/Account/Account.jsx
+++ e-commerce/src/pages/Account/Account.jsx
@@ -1,44 +1,62 @@
-import React from 'react';
-import { FcGoogle } from 'react-icons/fc';
-import { BsFacebook } from 'react-icons/bs';
-
-
-import './account.css';
+import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import { BsFacebook } from "react-icons/bs";
+import "./account.css";
+import { Link } from "react-router-dom";
 
 const Account = () => {
   return (
     <main className="myBody">
-        <div className="formContainer">          
-              <h1> Welcome Back!</h1>
-              
-              <form action="">    
-                  <div className='input-group'>
-                      <label className='label' htmlFor="Email">Email</label><p/>
-                      <input className="input-email" type="text" name="email" value="" placeholder='Enter your email address' /><p/>
-                  </div>              
-                  <button className='account-button continue'>Continue</button>
+      <div className="formContainer">
+        <h1> Welcome Back!</h1>
 
-                <p>OR</p>
-              
+        <form action="">
+          <div className="input-group">
+            <label className="label" htmlFor="Email">
+              Email
+            </label>
 
-                <button className='account-button '>
-                  <div className='facebook-button'>
-                    <BsFacebook style={{color: 'blue'}}/>
-                    Continue with Facebook
-                  </div>
-                </button><br />
-                <button className='account-button '>
-                  <div className='google-button'>
-                    <FcGoogle />
-                    Sign in with Google
-                  </div>
-                </button><br />
-                <a href='' className='get-started'> Don't have an account? Get started</a> <br /><br />
-                <p className='get-started2'>By clicking “Continue” you accept our <a href="">Terms of Service and Privacy Policy</a></p>
-              </form>    
-        </div>
+            <input
+              className="input-email"
+              type="text"
+              name="email"
+              value=""
+              placeholder="Enter your email address"
+            />
+            <p />
+          </div>
+          <button className="account-button continue">Continue</button>
+          <p>OR</p>
+          <button className="account-button ">
+            <div className="facebook-button">
+              <BsFacebook style={{ color: "blue" }} />
+              Continue with Facebook
+            </div>
+          </button>
+          <br />
+          <button className="account-button ">
+            <div className="google-button">
+              <FcGoogle />
+              Sign in with Google
+            </div>
+          </button>
+          <br />
+          <a href="" className="get-started">
+            {" "}
+            <Link to="/profile" className="link">
+              Don't have an account? Get started
+            </Link>
+          </a>{" "}
+          <br />
+          <br />
+          <p className="get-started2">
+            By clicking “Continue” you accept our{" "}
+            <a href="">Terms of Service and Privacy Policy</a>
+          </p>
+        </form>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Account
+export default Account;
