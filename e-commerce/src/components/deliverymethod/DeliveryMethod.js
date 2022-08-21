ea780a90ac67b1619609a7a7bdc5c1d661f10abe
Femi Olayode <folayode@wragbysolutions.com>
2022-08-21 08:30:25 +0100

    checkout page

---
+++ e-commerce/src/components/deliverymethod/DeliveryMethod.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const DeliveryMethod = () => {
+  return (
+    <Container>
+      <Row className="justify-content-md-center">
+        <Col xs lg="6">
+          <div>
+            <hr />
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col></Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default DeliveryMethod
--- e-commerce/src/pages/Checkout.jsx
+++ e-commerce/src/pages/Checkout.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import AddressDetails from '../components/addressdetails/AddressDetails'
+import CheckoutHeading from '../components/checkoutheading/CheckoutHeading'
+import CheckoutSubheading from '../components/checkoutsubheading/CheckoutSubheading'
+import DeliveryMethod from '../components/deliverymethod/DeliveryMethod'
 
 const Checkout = () => {
   return (
-    <div>Checkout</div>
+    <div>
+      <CheckoutHeading heading="checkout" />
+      <CheckoutSubheading title="Address details"></CheckoutSubheading>
+      <AddressDetails />
+      <CheckoutSubheading title="Delivery method"></CheckoutSubheading>
+      <DeliveryMethod />
+    </div>
   )
 }
 
