bd1a1bcac058a718f129ff5ffe29673cc50e1ae8
Femi Olayode <folayode@wragbysolutions.com>
2022-08-21 09:04:15 +0100

    delivery section

--- e-commerce/src/components/deliverymethod/DeliveryMethod.js
+++ e-commerce/src/components/deliverymethod/DeliveryMethod.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
+import '../deliverymethod/DeliveryMethod.css'
+import deliveryman from '../../images/delivery-man.png'
+import location from '../../images/location.png'
+import hand from '../../images/handtwo.png'
 
 const DeliveryMethod = () => {
   return (
@@ -11,8 +15,22 @@ const DeliveryMethod = () => {
           </div>
         </Col>
       </Row>
-      <Row>
-        <Col></Col>
+      <Row className="justify-content-md-center">
+        <Col className="deliverydeal">
+          <img src={deliveryman} />
+          <h6 className="font-weight-bold">Door Delivery</h6>
+          <p>Delivery N800. Delivery by weekend within the next 24hrs 20mins</p>
+        </Col>
+        <Col className="deliverydeal">
+          <img src={location} />
+          <h6 className="font-weight-bold">Door Delivery</h6>
+          <p>Delivery N800. Delivery by weekend within the next 24hrs 20mins</p>
+        </Col>
+        <Col className="deliverydeal">
+          <img src={hand} />
+          <h6 className="font-weight-bold">Door Delivery</h6>
+          <p>Delivery N800. Delivery by weekend within the next 24hrs 20mins</p>
+        </Col>
       </Row>
     </Container>
   )
