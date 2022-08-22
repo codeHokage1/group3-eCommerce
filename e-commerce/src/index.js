9a5e0c04baa3172202e06b821ad8df250bd8036b
sketodee <shobandejames@gmail.com>
2022-08-22 15:51:56 +0100

    admin page fixes

--- e-commerce/src/index.js
+++ e-commerce/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js"
 import "../node_modules/slick-carousel/slick/slick.css"
 import "../node_modules/slick-carousel/slick/slick-theme.css"
 
