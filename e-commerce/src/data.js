32a5f14c4f8a04311fe6ef5d511d16ab024d1c52
sketodee <shobandejames@gmail.com>
2022-08-23 08:41:10 +0100

    admin dashboard fixes

--- e-commerce/src/data.js
+++ e-commerce/src/data.js
@@ -8,9 +8,10 @@ import milo from './images/milo.jpg'; //for beverages
 import blueband from './images/blueband.jpg'; //for dairy
 
 
-const products = [
+const Categoriesdata = [
     {
-        beverage: [
+        name: "beverages",
+        products: [ 
             {
                 id: 1,
                 name: "Milo",
@@ -70,11 +71,12 @@ const products = [
                     rate: 4.6,
                     count: 400
                 }
-            }
+            },
         ]
     },
     {
-        dairy: [
+        name: "dairy",
+        products: [
             {
                 id: 6,
                 name: "Jago Milk",
@@ -134,11 +136,12 @@ const products = [
                     rate: 4.6,
                     count: 400
                 }
-            }
+            },
         ]
     },
     {
-        grains: [
+        name: "grains",
+        products: [
             {
                 id: 11,
                 name: "CAP rice",
@@ -174,11 +177,12 @@ const products = [
                     rate: 4.6,
                     count: 400
                 }
-            }
+            },
         ]
     },
     {
-        "canned foods": [
+        name: "canned foods",
+        products: [
             {
                 id: 14,
                 name: "Pringles",
@@ -214,283 +218,306 @@ const products = [
                     rate: 4.6,
                     count: 400
                 }
-            }
+            },
         ]
     },
     {
-        snacks: [
-            {
-                id: 17,
-                name: "Snickers",
-                price: 695,
-                description: "You can eat it",
-                category: "snacks",
-                image: snickers,
-                rating: {
-                    rate: 4.6,
-                    count: 400
-                }
-            },
-            {
-                id: 18,
-                name: "Crunchy",
-                price: 695,
-                description: "You can eat it",
-                category: "snacks",
-                image: snickers,
-                rating: {
-                    rate: 4.6,
-                    count: 400
-                }
-            },
-            {
-                id: 19,
-                name: "Oreos",
-                price: 695,
-                description: "You can eat it",
-                category: "snacks",
-                image: snickers,
-                rating: {
-                    rate: 4.6,
-                    count: 400
-                }
+        name: "canned foods",
+        products: [ {
+            id: 17,
+            name: "Snickers",
+            price: 695,
+            description: "You can eat it",
+            category: "snacks",
+            image: snickers,
+            rating: {
+                rate: 4.6,
+                count: 400
             }
-        ]
-    }
+        },
+        {
+            id: 18,
+            name: "Crunchy",
+            price: 695,
+            description: "You can eat it",
+            category: "snacks",
+            image: snickers,
+            rating: {
+                rate: 4.6,
+                count: 400
+            }
+        },
+        {
+            id: 19,
+            name: "Oreos",
+            price: 695,
+            description: "You can eat it",
+            category: "snacks",
+            image: snickers,
+            rating: {
+                rate: 4.6,
+                count: 400
+            }
+        }]
+    },
+
 ]
 
-export default products;
+export default Categoriesdata;
 
-
-
-
-// [
+// const products = [
 //     {
-//         id: 1,
-//         name: "Milo",
-//         price: 109.95,
-//         description: "You can eat it",
-//         category: "beverages",
-//         image: milo,
-//         rating: {
-//             rate: 3.9,
-//             count: 120
-//         }
+//         beverage: [
+//             {
+//                 id: 1,
+//                 name: "Milo",
+//                 price: 109.95,
+//                 description: "You can eat it",
+//                 category: "beverages",
+//                 image: milo,
+//                 rating: {
+//                     rate: 3.9,
+//                     count: 120
+//                 }
+//             },
+//             {
+//                 id: 2,
+//                 name: "Bournvita",
+//                 price: 22.3,
+//                 description: "You can eat it",
+//                 category: "beverages",
+//                 image: milo,
+//                 rating: {
+//                     rate: 4.1,
+//                     count: 259
+//                 }
+//             },
+//             {
+//                 id: 3,
+//                 name: "Nescafe",
+//                 price: 55.99,
+//                 description: "You can eat it",
+//                 category: "beverages",
+//                 image: milo,
+//                 rating: {
+//                     rate: 4.7,
+//                     count: 500
+//                 }
+//             },
+//             {
+//                 id: 4,
+//                 name: "Ovaltine",
+//                 price: 15.99,
+//                 description: "You can eat it",
+//                 category: "beverages",
+//                 image: milo,
+//                 rating: {
+//                     rate: 2.1,
+//                     count: 430
+//                 }
+//             },
+//             {
+//                 id: 5,
+//                 name: "Cowbell Choco",
+//                 price: 695,
+//                 description: "You can eat it",
+//                 category: "beverages",
+//                 image: milo,
+//                 rating: {
+//                     rate: 4.6,
+//                     count: 400
+//                 }
+//             }
+//         ]
 //     },
 //     {
-//         id: 2,
-//         name: "Bournvita",
-//         price: 22.3,
-//         description: "You can eat it",
-//         category: "beverages",
-//         image: milo,
-//         rating: {
-//             rate: 4.1,
-//             count: 259
-//         }
+//         dairy: [
+//             {
+//                 id: 6,
+//                 name: "Jago Milk",
+//                 price: 695,
+//                 description: "You can eat it",
+//                 category: "dairy",
+//                 image: blueband,
+//                 rating: {
+//                     rate: 4.6,
+//                     count: 400
+//                 }
+//             },
+//             {
+//                 id: 7,
+//                 name: "Dano Milk",
+//                 price: 695,
+//                 description: "You can eat it",
+//                 category: "dairy",
+//                 image: blueband,
+//                 rating: {
+//                     rate: 4.6,
+//                     count: 400
+//                 }
+//             },
+//             {
+//                 id: 8,
+//                 name: "Loya Milk",
+//                 price: 695,
+//                 description: "You can eat it",
+//                 category: "dairy",
+//                 image: blueband,
+//                 rating: {
+//                     rate: 4.6,
+//                     count: 400
+//                 }
+//             },
+//             {
+//                 id: 9,
+//                 name: "Big Tin Milk",
+//                 price: 695,
+//                 description: "You can eat it",
+//                 category: "dairy",
+//                 image: blueband,
+//                 rating: {
+//                     rate: 4.6,
+//                     count: 400
+//                 }
+//             },
+//             {
+//                 id: 10,
+//                 name: "Big Loya Milk",
+//                 price: 695,
+//                 description: "You can eat it",
+//                 category: "dairy",
+//                 image: blueband,
+//                 rating: {
+//                     rate: 4.6,
+//                     count: 400
+//                 }
+//             }
+//         ]
 //     },
 //     {
-//         id: 3,
-//         name: "Nescafe",
-//         price: 55.99,
-//         description: "You can eat it",
-//         category: "beverages",
-//         image: milo,
-//         rating: {
-//             rate: 4.7,
-//             count: 500
-//         }
+//         grains: [
+//             {
+//                 id: 11,
+//                 name: "CAP rice",
+//                 price: 695,
+//                 description: "You can eat it",
+//                 category: "grains",
+//                 image: rice,
+//                 rating: {
+//                     rate: 4.6,
+//                     count: 400
+//                 }
+//             },
+//             {
+//                 id: 12,
+//                 name: "Stallion Rice",
+//                 price: 695,
+//                 description: "You can eat it",
+//                 category: "grains",
+//                 image: rice,
+//                 rating: {
+//                     rate: 4.6,
+//                     count: 400
+//                 }
+//             },
+//             {
+//                 id: 13,
+//                 name: "Nigerian Rice",
+//                 price: 695,
+//                 description: "You can eat it",
+//                 category: "grains",
+//                 image: rice,
+//                 rating: {
+//                     rate: 4.6,
+//                     count: 400
+//                 }
+//             }
+//         ]
 //     },
 //     {
-//         id: 4,
-//         name: "Ovaltine",
-//         price: 15.99,
-//         description: "You can eat it",
-//         category: "beverages",
-//         image: milo,
-//         rating: {
-//             rate: 2.1,
-//             count: 430
-//         }
+//         "canned foods": [
+//             {
+//                 id: 14,
+//                 name: "Pringles",
+//                 price: 695,
+//                 description: "You can eat it",
+//                 category: "canned food",
+//                 image: pringles,
+//                 rating: {
+//                     rate: 4.6,
+//                     count: 400
+//                 }
+//             },
+//             {
+//                 id: 15,
+//                 name: "Titus Sardine",
+//                 price: 695,
+//                 description: "You can eat it",
+//                 category: "canned food",
+//                 image: pringles,
+//                 rating: {
+//                     rate: 4.6,
+//                     count: 400
+//                 }
+//             },
+//             {
+//                 id: 16,
+//                 name: "HotDogs",
+//                 price: 695,
+//                 description: "You can eat it",
+//                 category: "canned food",
+//                 image: pringles,
+//                 rating: {
+//                     rate: 4.6,
+//                     count: 400
+//                 }
+//             }
+//         ]
 //     },
 //     {
-//         id: 5,
-//         name: "Cowbell Choco",
-//         price: 695,
-//         description: "You can eat it",
-//         category: "beverages",
-//         image: milo,
-//         rating: {
-//             rate: 4.6,
-//             count: 400
-//         }
-//     },
-//     {
-//         id: 6,
-//         name: "Jago Milk",
-//         price: 695,
-//         description: "You can eat it",
-//         category: "dairy",
-//         image: blueband,
-//         rating: {
-//             rate: 4.6,
-//             count: 400
-//         }
-//     },
-//     {
-//         id: 7,
-//         name: "Dano Milk",
-//         price: 695,
-//         description: "You can eat it",
-//         category: "dairy",
-//         image: blueband,
-//         rating: {
-//             rate: 4.6,
-//             count: 400
-//         }
-//     },
-//     {
-//         id: 8,
-//         name: "Loya Milk",
-//         price: 695,
-//         description: "You can eat it",
-//         category: "dairy",
-//         image: blueband,
-//         rating: {
-//             rate: 4.6,
-//             count: 400
-//         }
-//     },
-//     {
-//         id: 9,
-//         name: "Big Tin Milk",
-//         price: 695,
-//         description: "You can eat it",
-//         category: "dairy",
-//         image: blueband,
-//         rating: {
-//             rate: 4.6,
-//             count: 400
-//         }
-//     },
-//     {
-//         id: 10,
-//         name: "Big Loya Milk",
-//         price: 695,
-//         description: "You can eat it",
-//         category: "dairy",
-//         image: blueband,
-//         rating: {
-//             rate: 4.6,
-//             count: 400
-//         }
-//     },
-//     {
-//         id: 11,
-//         name: "CAP rice",
-//         price: 695,
-//         description: "You can eat it",
-//         category: "grains",
-//         image: rice,
-//         rating: {
-//             rate: 4.6,
-//             count: 400
-//         }
-//     },
-//     {
-//         id: 12,
-//         name: "Stallion Rice",
-//         price: 695,
-//         description: "You can eat it",
-//         category: "grains",
-//         image: rice,
-//         rating: {
-//             rate: 4.6,
-//             count: 400
-//         }
-//     },
-//     {
-//         id: 13,
-//         name: "Nigerian Rice",
-//         price: 695,
-//         description: "You can eat it",
-//         category: "grains",
-//         image: rice,
-//         rating: {
-//             rate: 4.6,
-//             count: 400
-//         }
-//     },
-//     {
-//         id: 14,
-//         name: "Pringles",
-//         price: 695,
-//         description: "You can eat it",
-//         category: "canned food",
-//         image: pringles,
-//         rating: {
-//             rate: 4.6,
-//             count: 400
-//         }
-//     },
-//     {
-//         id: 15,
-//         name: "Titus Sardine",
-//         price: 695,
-//         description: "You can eat it",
-//         category: "canned food",
-//         image: pringles,
-//         rating: {
-//             rate: 4.6,
-//             count: 400
-//         }
-//     },
-//     {
-//         id: 16,
-//         name: "HotDogs",
-//         price: 695,
-//         description: "You can eat it",
-//         category: "canned food",
-//         image: pringles,
-//         rating: {
-//             rate: 4.6,
-//             count: 400
-//         }
-//     },
-//     {
-//         id: 17,
-//         name: "Snickers",
-//         price: 695,
-//         description: "You can eat it",
-//         category: "snacks",
-//         image: snickers,
-//         rating: {
-//             rate: 4.6,
-//             count: 400
-//         }
-//     },
-//     {
-//         id: 18,
-//         name: "Crunchy",
-//         price: 695,
-//         description: "You can eat it",
-//         category: "snacks",
-//         image: snickers,
-//         rating: {
-//             rate: 4.6,
-//             count: 400
-//         }
-//     },
-//     {
-//         id: 19,
-//         name: "Oreos",
-//         price: 695,
-//         description: "You can eat it",
-//         category: "snacks",
-//         image: snickers,
-//         rating: {
-//             rate: 4.6,
-//             count: 400
-//         }
+//         snacks: [
+//             {
+//                 id: 17,
+//                 name: "Snickers",
+//                 price: 695,
+//                 description: "You can eat it",
+//                 category: "snacks",
+//                 image: snickers,
+//                 rating: {
+//                     rate: 4.6,
+//                     count: 400
+//                 }
+//             },
+//             {
+//                 id: 18,
+//                 name: "Crunchy",
+//                 price: 695,
+//                 description: "You can eat it",
+//                 category: "snacks",
+//                 image: snickers,
+//                 rating: {
+//                     rate: 4.6,
+//                     count: 400
+//                 }
+//             },
+//             {
+//                 id: 19,
+//                 name: "Oreos",
+//                 price: 695,
+//                 description: "You can eat it",
+//                 category: "snacks",
+//                 image: snickers,
+//                 rating: {
+//                     rate: 4.6,
+//                     count: 400
+//                 }
+//             }
+//         ]
 //     }
 // ]
+
+// export default products;
+
+
+
+
